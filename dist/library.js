116e30fbc111b7454bcabf0476f361c289d26ecb
Geraldi Sutanto <contact@geraldisutanto.com>
2021-11-02 08:59:31 +0700

    update lib

--- dist/library.js
+++ dist/library.js
@@ -4805,7 +4805,7 @@ var uuid = function uuid() {
 
 var normalize = function normalize(url) {
   url = url.startsWith("www.") ? url.slice(4) : url;
-  url = url.endsWith("/") ? url.slice(0, 1) : url;
+  url = url.endsWith("/") ? url.slice(0, -1) : url;
   return url;
 };
 
